9a231ba2dd487a2a731b5292cd1e777a14fdff45
adammushy <adamprosper99@gmail.com>
2024-01-12 23:30:11 +0300

    updates on statics

--- DatingApp/settings.py
+++ DatingApp/settings.py
@@ -160,6 +160,6 @@ CHANNEL_LAYERS = {
     },
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
