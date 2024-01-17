68de11b243796ba3278df0e3a0711ff2ec524543
adammushy <adamprosper99@gmail.com>
2024-01-18 02:12:22 +0300

    updates

--- DatingApp/settings.py
+++ DatingApp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*2kcc=%%ikue%ieqfdp)5v556&t@-p^_xzhgs2k-k8hk!g)qc&'
-
+# SECRET_KEY = 'django-insecure-*2kcc=%%ikue%ieqfdp)5v556&t@-p^_xzhgs2k-k8hk!g)qc&'
+SECRET_KEY =os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get("DEBUG","False").lower() =="true"
 
-ALLOWED_HOSTS = ["*"]
+
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+
 
 
 # Application definition
@@ -105,6 +110,12 @@ DATABASES = {
     }
 }
 
+database_url = os.environ.get("DATABASE_URL")
+
+# DATABASES["default"]= dj_database_url.parse("postgres://datingapp_vw2m_user:SmV1FDPxHJh1IhTHDuShDeGmk8vB6H1L@dpg-cmjra5fqd2ns73bkvgdg-a.oregon-postgres.render.com/datingapp_vw2m")
+DATABASES["default"]= dj_database_url.parse(database_url )
+
+# postgres://datingapp_vw2m_user:SmV1FDPxHJh1IhTHDuShDeGmk8vB6H1L@dpg-cmjra5fqd2ns73bkvgdg-a/datingapp_vw2m
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
