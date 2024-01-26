4455f0e593036a633722fa3b3ca928f21cdbb5e2
adammushy <adamprosper99@gmail.com>
2024-01-26 19:53:21 +0300

    updates on user

--- DatingApp/settings.py
+++ DatingApp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,16 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-*2kcc=%%ikue%ieqfdp)5v556&t@-p^_xzhgs2k-k8hk!g)qc&'
-SECRET_KEY =os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-*2kcc=%%ikue%ieqfdp)5v556&t@-p^_xzhgs2k-k8hk!g)qc&'
+
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get("DEBUG","False").lower() =="true"
+DEBUG = True
 
-
-# ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -110,12 +105,6 @@ DATABASES = {
     }
 }
 
-database_url = os.environ.get("DATABASE_URL")
-
-# DATABASES["default"]= dj_database_url.parse("postgres://datingapp_vw2m_user:SmV1FDPxHJh1IhTHDuShDeGmk8vB6H1L@dpg-cmjra5fqd2ns73bkvgdg-a.oregon-postgres.render.com/datingapp_vw2m")
-DATABASES["default"]= dj_database_url.parse(database_url )
-
-# postgres://datingapp_vw2m_user:SmV1FDPxHJh1IhTHDuShDeGmk8vB6H1L@dpg-cmjra5fqd2ns73bkvgdg-a/datingapp_vw2m
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
--- user_management/urls.py
+++ user_management/urls.py
@@ -5,10 +5,11 @@ app_name = 'user_management'
 urlpatterns = [
     path('register', RegisterUser.as_view()),
     path('login', LoginView.as_view()),
-    path('user-information/<slug:query_type>', UserInformation.as_view()),
+    path('user-information<slug:query_type>', UserInformation.as_view()),
     path('change-password', ChangePasswordView.as_view()),
     path('update-user', UpdateUserView.as_view()),
     path('get-create-account/<slug:userId>', AccountView.as_view()),
     path('delete-update-account', DeleteUpdateAccount.as_view()),
     path('change-user-status', ChangeUserStatus.as_view()),
+    path('all-account/<slug:accountId>', AllAccountView.as_view()),
 ]
